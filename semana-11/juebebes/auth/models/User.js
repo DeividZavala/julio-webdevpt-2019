0c4a6668781dd758e4c7ca288e650f4fbb9ffb2a
DeividZavala <crusedmoss@gmail.com>
2019-09-26 20:36:16 -0500

    auth validation

--- semana-11/juebebes/auth/models/User.js
+++ semana-11/juebebes/auth/models/User.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
-  }
-});
+  { timestamps }
+);
 
 module.exports = model("User", userSchema);
--- semana-11/juebebes/auth/routes/auth.js
+++ semana-11/juebebes/auth/routes/auth.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/register", (req, res, next) => {
+router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+
+  let error;
+  const { password, email } = req.body;
+
+  if (!email || !password) {
+    error = "No enviaste un email con contraseña valida";
+    return res.render("register", { error });
+  }
+
+  if (password !== req.body["confirm-pass"]) {
+    error = "Las contraseñas no son las mismas";
+    return res.render("register", { error });
+  }
 });
 
 module.exports = router;
