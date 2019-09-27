36758888329462b627cf666775f6700ccfe0e371
DeividZavala <crusedmoss@gmail.com>
2019-09-26 20:08:34 -0500

    auth form

---
+++ semana-11/juebebes/auth/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = model("User", userSchema);
