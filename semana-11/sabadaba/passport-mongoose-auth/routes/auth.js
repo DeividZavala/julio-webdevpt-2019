7c382eb53f884098403180a6f742baf1f9979070
DeividZavala <crusedmoss@gmail.com>
2019-09-28 16:15:23 -0500

    roles :hankey:

--- semana-11/sabadaba/passport-mongoose-auth/routes/auth.js
+++ semana-11/sabadaba/passport-mongoose-auth/routes/auth.js
@@ -1,12 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("../helpers/passport");
+const User = require("../models/User");
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.post("/login", (req, res) => {
+  passport.authenticate("local", (err, user, info = {}) => {
+    const { message: error } = info;
+    if (error) {
+      return res.render("login", { error });
+    }
+
+    req.login(user, err => {
+      res.redirect("/");
+    });
+  })(req, res);
+});
+
 router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  const { password, email } = req.body;
+
+  let error;
+
+  if (password !== req.body["confirm-pass"]) {
+    error = "Las contras no son las mismas";
+    return res.render("register", { error });
+  }
+
+  if (!email || !password) {
+    error = "Email o contraseña incorrectos";
+    return res.render("register", { error });
+  }
+
+  User.register({ email }, password)
+    .then(user => {
+      req.login(user, err => {
+        res.redirect("/");
+      });
+    })
+    .catch(error => {
+      res.render("register", { error });
+    });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
 });
 
 module.exports = router;
--- semana-11/sabadaba/passport-mongoose-auth/routes/index.js
+++ semana-11/sabadaba/passport-mongoose-auth/routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const isAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect("/login");
+};
+
+const checkRole = roles => {
+  return (req, res, next) => {
+    if (roles.includes(req.user.role)) {
+      return next();
+    }
+    return res.redirect("/");
+  };
+};
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", isAuth, (req, res, next) => {
+  const { user } = req;
+  res.render("index", { user });
+});
+
+router.get("/admin", isAuth, checkRole(["admin"]), (req, res) => {
+  const { user } = req;
+  res.render("admin", { user });
 });
 
 module.exports = router;
