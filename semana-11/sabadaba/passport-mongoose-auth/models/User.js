4a2d8aecee19eecc8477ea9e1da5ac83901ea282
DeividZavala <crusedmoss@gmail.com>
2019-09-28 15:03:44 -0500

    passport local mongoose :alien:

---
+++ semana-11/sabadaba/passport-mongoose-auth/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      enum: ["admin", "user"],
+      default: "user"
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email",
+  hashField: "password"
+});
+
+module.exports = model("User", userSchema);
