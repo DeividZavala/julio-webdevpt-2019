eed15ae9ac46beb72c9064a6a4d5c5b99897336f
DeividZavala <crusedmoss@gmail.com>
2019-10-03 20:48:27 -0500

    form :hankey:

--- semana-12/jueves/app.js
+++ semana-12/jueves/app.js
@@ -1,3 +1,22 @@
-axios.get("https://rickandmortyapi.com/api/character").then(res => {
-  console.log(res);
-});
+const list = document.getElementById("list");
+const span = document.querySelector("span");
+const btn = document.getElementById("load");
+
+let next = "";
+let characters = [];
+
+const getCharacters = (url = "https://rickandmortyapi.com/api/character") => {
+  axios.get(url).then(res => {
+    console.log(res);
+    const { results, info } = res.data;
+    next = info.next;
+    characters = [...characters, ...results];
+    results.forEach(character => {
+      span.innerText = res.data.info.count;
+      list.innerHTML += `<li>${character.name}</li>`;
+    });
+  });
+};
+
+getCharacters();
+btn.onclick = () => getCharacters(next);
