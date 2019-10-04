dec73934c05728aac01bb0c4dc5c20f7af766a38
DeividZavala <crusedmoss@gmail.com>
2019-10-03 21:12:32 -0500

    card :hankey:

--- semana-12/jueves/app.js
+++ semana-12/jueves/app.js
@@ -1,6 +1,9 @@
 const list = document.getElementById("list");
 const span = document.querySelector("span");
 const btn = document.getElementById("load");
+const query = document.querySelector("input");
+const search = document.getElementById("search");
+const detail = document.getElementById("detail");
 
 let next = "";
 let characters = [];
@@ -18,5 +21,34 @@ const getCharacters = (url = "https://rickandmortyapi.com/api/character") => {
   });
 };
 
+const getCharacter = (id = 1) => {
+  axios.get(`https://rickandmortyapi.com/api/character/${id}`).then(res => {
+    console.log(res);
+    const { data: character } = res;
+    detail.innerHTML = `
+    <div class="uk-card uk-card-default uk-child-width-1-1 uk-margin" uk-grid>
+        <div class="uk-card-media-left uk-cover-container">
+            <img src="${character.image}" alt="" uk-cover>
+            <canvas width="600" height="400"></canvas>
+        </div>
+        <div>
+            <div class="uk-card-body">
+                <h3 class="uk-card-title">${
+                  character.name
+                } <span class="uk-card-badge uk-label ${
+      character.species === "Human" ? "" : "uk-label-danger"
+    }">${character.species}</span></h3>
+            </div>
+        </div>
+    </div>
+    
+    `;
+  });
+};
+
 getCharacters();
+getCharacter(2);
 btn.onclick = () => getCharacters(next);
+search.onclick = () => {
+  getCharacter(query.value);
+};
