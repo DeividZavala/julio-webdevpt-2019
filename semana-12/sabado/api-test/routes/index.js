4bdb9ce1e4fa1abf8b15c10166044585f3af3f96
DeividZavala <crusedmoss@gmail.com>
2019-10-05 12:11:24 -0500

    handling errors

--- semana-12/sabado/api-test/routes/index.js
+++ semana-12/sabado/api-test/routes/index.js
@@ -12,31 +12,61 @@ const Media = require("../models/Media");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  Media.find().then(media => {
-    res.status(200).json({ media });
-  });
+  const countPromise = Media.count();
+  const mediaPromise = Media.find();
+  Promise.all([countPromise, mediaPromise])
+    .then(results => {
+      const [count, media] = results;
+      res.status(200).json({ media, count });
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Media.findById(id)
+    .then(media => {
+      res.status(200).json({ media });
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
 });
 
 router.post("/", (req, res) => {
   const { body: media } = req;
-  Media.create(media).then(media => {
-    res.status(200).json({ media });
-  });
+  Media.create(media)
+    .then(media => {
+      res.status(200).json({ media });
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
 });
 
 router.patch("/:id", (req, res) => {
   const { body: media } = req;
   const { id } = req.params;
-  Media.findByIdAndUpdate(id, { $set: media }, { new: true }).then(media => {
-    res.status(200).json({ media });
-  });
+  Media.findByIdAndUpdate(id, { $set: media }, { new: true })
+    .then(media => {
+      res.status(200).json({ media });
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
 });
 
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  Media.findByIdAndDelete(id).then(media => {
-    res.status(200).json({ media });
-  });
+  Media.findByIdAndDelete(id)
+    .then(media => {
+      res.status(200).json({ media });
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
 });
 
 module.exports = router;
